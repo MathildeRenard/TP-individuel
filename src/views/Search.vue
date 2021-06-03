277baa9fe0dcd1bc72921113c3c4561e59a9fd2a
Mathilde Renard <mathilderenard626@gmail.com>
2021-06-03 14:25:00 +0200

    Ajout d'une nouvelle ville dans la base de données

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -7,11 +7,14 @@
 </template>
 
 <script lang="ts">
-    import {defineComponent} from "vue";
-    import {IonSearchbar, IonButton, toastController } from "@ionic/vue";
-    import Navbar from "@/views/Navbar.vue";
-    import weatherService from "@/services/weatherService";
-
+import firebase from 'firebase/app';
+import 'firebase/firestore'
+import { DATABASE_CONFIGURATION } from '@/config.js';
+import {defineComponent} from "vue";
+import {IonSearchbar, IonButton, toastController } from "@ionic/vue";
+import Navbar from "@/views/Navbar.vue";
+import weatherService from "@/services/weatherService";
+const db = firebase.firestore();
     export default defineComponent ({
         name: "Search",
         data() {
@@ -33,6 +36,10 @@
                 this.currentWeather = await weatherService.getCityName(this.city);
                     const msg = await this.currentWeather.cod === '200' ? 'Votre saisie a bien été enregistrée' : 'erreur rencontrée';
                     this.PrintToast(msg);
+
+              //ajout d'une nouvelle ville dans la liste
+              await db.collection('cities').add({
+                name: this.city})
             },
             async PrintToast(msg: string){
                 const toast = await toastController
