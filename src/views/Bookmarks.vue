1b5c25558d6f69f16e4156e03de961a76f90e513
Mathilde Renard <mathilderenard626@gmail.com>
2021-06-03 14:29:42 +0200

    Ajout de composants

--- src/views/Bookmarks.vue
+++ src/views/Bookmarks.vue
@@ -2,11 +2,6 @@
     <ion-page>
         <Navbar :title="'bookmarks'">
         </Navbar>
-
-        <ion-button>
-            <ion-icon slot="icon-only" :icon="arrowUndo"></ion-icon>
-        </ion-button>
-
         <ion-content v-if="cities.length>0">
             <BookmarkedElement v-for="(city,index) in cities" :key="index" :city="city.name" :tempMorning="'12,5°C'" :tempAfternoon="'21,5°C'">
             </BookmarkedElement>
@@ -17,31 +12,26 @@
 <script>
     import{
         IonPage,
-        IonContent,
-        IonIcon,
-        IonButton
+        IonContent
     } from '@ionic/vue'
 
     import firebase from 'firebase/app'
     import 'firebase/firestore'
     import { DATABASE_CONFIGURATION } from '@/config.js';
+    export const db = firebase.initializeApp(DATABASE_CONFIGURATION).firestore()
 
     import Navbar from './Navbar';
     import BookmarkedElement from "@/views/BookmarkedElement";
 
-    import {arrowUndo} from 'ionicons/icons';
-
     import { defineComponent } from 'vue';
 
-    export const db = firebase.initializeApp(DATABASE_CONFIGURATION).firestore()
+
 
     export default defineComponent ({
         name: "Bookmarks",
         components : {
             IonPage,
             IonContent,
-            IonIcon,
-            IonButton,
             Navbar,
             BookmarkedElement
         },
@@ -50,17 +40,15 @@
                 cities : []
             }
         },
-        setup() {
-            return arrowUndo
-        },
         firestore: {
             cities : db.collection('cities'),
         },
 
+
     })
 
 </script>
-lu
+
 <style scoped>
 
 </style>
